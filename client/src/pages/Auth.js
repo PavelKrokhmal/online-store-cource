a7ed8e742d2b9c66a1a4a6e358273a067e08911c
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-01 15:46:53 +0300

    add Auth

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import {Container, Form, Card, Button, Row} from 'react-bootstrap'
+import {NavLink, useLocation} from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 
 function Auth() {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}>
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Authorization" : "Registration"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control placeholder="Input your email..."
+                        type="email" 
+                        className="mt-4"/>
+                    <Form.Control placeholder="Input your password..." 
+                        type="password"
+                        className="mt-2"/>
+                    <Row className="d-flex justify-content-between mt-4 pl-3 pr-3">
+                        {isLogin ? 
+                            <div>
+                                No account? <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Have account? <NavLink to={LOGIN_ROUTE}>Log in</NavLink>
+                            </div>
+                        }
+                        <Button variant={"outline-success"}>
+                           {isLogin ? "Log in" : "Registration"}
+                        </Button>
+                    </Row>
+                </Form>
+
+            </Card>
+        </Container>
     )
 }
 
